e1a5f371a63773cb04bafb1ee502cffe72ff950a
Thevoyboy <harsha.vr019@gmail.com>
2017-04-13 17:02:46 -0400

    Initial commit

---
+++ src/main/java/IO/SpringBootStarter/CourseApiApp.java
@@ -0,0 +1,14 @@
+package IO.SpringBootStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by harsh on 4/13/2017.
+ */
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class,args);
+    }
+}
